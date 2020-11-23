807fb3757008fc60c0cfc159c83ac40837deb099
Miguel Angel Cabrero <miguelangelcabrero@gmail.com>
2020-11-23 20:59:11 +0100

    Actualizado ejercicio 126

--- Q126/Program.cs
+++ Q126/Program.cs
@@ -15,35 +15,39 @@ namespace Q126
             Console.WriteLine(FormatCoins_A("JOSÉ PEREZ", 0));
             Console.WriteLine(FormatCoins_A("JOSÉ PEREZ", 999));
             Console.WriteLine(FormatCoins_A("JOSÉ PEREZ", 1000));
+            Console.WriteLine(FormatCoins_A("JOSÉ PEREZ", 40));
             Console.WriteLine("OPCION B:");
             Console.WriteLine(FormatCoins_B("JOSÉ PEREZ", 5));
             Console.WriteLine(FormatCoins_B("JOSÉ PEREZ", 0));
             Console.WriteLine(FormatCoins_B("JOSÉ PEREZ", 999));
             Console.WriteLine(FormatCoins_B("JOSÉ PEREZ", 1000));
             //Console.WriteLine("OPCION C:");
-            //Console.WriteLine(FormatCoins_C("JOSÉ PEREZ", 5));
-            //Console.WriteLine(FormatCoins_C("JOSÉ PEREZ", 0));
-            //Console.WriteLine("OPCION D:");
-            //Console.WriteLine(FormatCoins_D("JOSÉ PEREZ", 5));
-            //Console.WriteLine(FormatCoins_D("JOSÉ PEREZ", 0));
+            //Console.WriteLine(FormatCoins_C("josé perez", 5));
+            //Console.WriteLine(FormatCoins_C("josé perez", 0));
+            Console.WriteLine("OPCION D:");
+            Console.WriteLine(FormatCoins_D("JOSÉ PEREZ", 5));
+            Console.WriteLine(FormatCoins_D("JOSÉ PEREZ", 0));
+            Console.WriteLine(FormatCoins_D("JOSÉ PEREZ", 40));
+
             Console.ReadLine();
         }
         static string FormatCoins_A(string name, int coins)
         {
-            return String.Format("Player {0},collected {1}", name, coins.ToString("###0"));
+            return String.Format("Player {0},collected {1} coins", name, coins.ToString("###0"));
         }
 
         static string FormatCoins_B(string name, int coins)
         {
-            return String.Format("Player {0},collected {1:000#} coins.", name, coins);
+            return String.Format("Player {0} collected {1:000#} coins.", name, coins);
         }
         //static string FormatCoins_C(string name, int coins)
         //{
-        //    return String.Format("Player {name},collected {coins.ToString('000')} coins");
+        //    return string.Format("Player {name} collected {coins.ToString('000')} coins");
         //}
-        //static string FormatCoins_D(string name, int coins)
-        //{
-        //    return String.Format("Player {1},collected {2:D3} coins", name, coins);
-        //}
+
+        static string FormatCoins_D(string name, int coins)
+        {
+            return string.Format("Player {0} collected {1:D1} coins", name, coins);
+        }
     }
 }
